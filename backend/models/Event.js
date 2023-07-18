6099591a409c029465cb2af2164005b943e75fe9
khoaihps <khoadoan2472003@gmail.com>
2023-07-18 22:41:26 +0700

    Update model

---
+++ backend/models/Event.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Event Schema
+const eventSchema = new Schema({
+    event_id: { type: String, required: true },
+    name: { type: String, required: true },
+    customer_id: { type: Schema.Types.ObjectId, ref: 'Customer', required: true },
+    deadline: { type: Date, required: true },
+    place: { type: String, required: true },
+    type_of_event: {
+      type: String,
+      enum: ['Social', 'Educational', 'Community', 'Cultural Events'],
+      required: true
+    },
+    description: { type: String, required: true },
+    date_proposed: { type: Date, required: true },
+    last_modified: { type: Date, default: Date.now },
+    status: {
+        type: String,
+        enum: ['pending', 'open', 'closed'],
+        required: true
+    }
+});
+
+// Task Schema
+const taskSchema = new Schema({
+    task_id: { type: String, required: true },
+    name: { type: String, required: true },
+    deadline: { type: Date, required: true },
+    budget: { type: Number, required: true },
+    description: { type: String, required: true },
+    department_involved: { type: String, required: true },
+    event_id: { type: Schema.Types.ObjectId, ref: 'Event', required: true }
+  });
+
+// Task Assign Schema
+const taskAssignSchema = new Schema({
+    task_id: { type: Schema.Types.ObjectId, ref: 'Task', required: true },
+    t_member_id: { type: Schema.Types.ObjectId, ref: 'TeamMember', required: true }
+});
+
+// Event Register Schema
+const eventRegisterSchema = new Schema({
+    event_id: { type: Schema.Types.ObjectId, ref: 'Event', required: true },
+    t_member_id: { type: Schema.Types.ObjectId, ref: 'TeamMember', required: true }
+});
+
+
+const Event = mongoose.model('Event', eventSchema);
+const Task = mongoose.model('Task', taskSchema);
+const TaskAssign = mongoose.model('TaskAssign', taskAssignSchema);
+const EventRegister = mongoose.model('EventRegister', eventRegisterSchema);
+
+module.exports = {
+    Event,
+    Task,
+    TaskAssign,
+    EventRegister
+};
+
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,57 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['manager', 'employee', 'customer'], required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['manager', 'employee', 'customer'], required: true },
 });
 
-const User = mongoose.model('User', userSchema);
+// Customer Schema
+const customerSchema = new Schema({
+    customer_id: { type: Schema.Types.ObjectId, default: mongoose.Types.ObjectId },
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    DOB: { type: Date, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    address: { type: String, required: true }
+});
 
-module.exports = User;
+// Manager Schema
+const managerSchema = new Schema({
+    manager_id: { type: Schema.Types.ObjectId, default: mongoose.Types.ObjectId },
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    DOB: { type: Date, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    address: { type: String, required: true }
+});
+
+// Employee Schema
+const employeeSchema = new Schema({
+    employee_id: { type: String, required: true },
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    DOB: { type: Date, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    address: { type: String, required: true },
+    department: { type: String, enum: ['Operation', 'Media', 'Program - MC', 'Content Planning'], required: true }
+});
+
+
+const User = mongoose.model('User', userSchema);
+const Customer = mongoose.model('Customer', customerSchema);
+const Manager = mongoose.model('Manager', managerSchema);
+const Employee = mongoose.model('Employee', employeeSchema);
+
+module.exports = {
+    User,
+    Customer,
+    Manager,
+    Employee,
+};
