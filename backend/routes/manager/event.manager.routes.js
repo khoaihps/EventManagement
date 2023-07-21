913353590dab63c401a0b424b7155c3b8906f1d0
khoaihps <khoadoan2472003@gmail.com>
2023-07-21 14:27:39 +0700

    add features eventController

--- backend/routes/manager/event.manager.routes.js
+++ backend/routes/manager/event.manager.routes.js
@@ -1,8 +1,13 @@
-const { allEvents } = require('../../controllers/event.controller');
+const { allEvents, eventDetail } = require('../../controllers/event.controller');
+const express = require("express");
+const { authenticateManager } = require('../../middlewares/authMiddleware');
+const { allTaskOfEvent } = require('../../controllers/event.controller');
+const router = express.Router();
 
-module.exports = (app) => {
-    app.get('/manager/events', allEvents);
-    app.get('/manager/event101', (req, res) => {
-        res.status(200).send("Successful");
-    })
-}
+router.use(authenticateManager);
+
+router.get('/all', allEvents);
+router.get('/:eventId', eventDetail);
+router.get('/:eventId/task', allTaskOfEvent);
+
+module.exports = router;
