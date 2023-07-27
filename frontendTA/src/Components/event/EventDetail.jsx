19f2d69a3180bc9a4179859e1399bed6db864de0
haidohong <dohaimail@gmail.com>
2023-07-27 22:52:40 +0700

    Remove duplicate files

--- frontendTA/src/Components/event/EventDetail.jsx
+++ frontendTA/src/Components/event/EventDetail.jsx
@@ -1,11 +1,25 @@
-import {useParams} from "react-router-dom";
+import {useLoaderData, useParams} from "react-router-dom";
 import Header from "../environment/Header";
 import Sidebar from "../environment/Sidebar";
 import EventInfo from "./EventInfo";
 import '../style/eventDetail.css'
+import EventService from "../../services/event.service";
+import TaskService from "../../services/task.service";
+
+export const loader = async ({ params }) => {
+    try {
+        const details = await EventService.getEventInfo(params.eventID);
+        const tasks = await TaskService.getAllTaskEvent(params.eventID);
+        return {details, tasks};
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+    return null;
+}
 
 export const EventDetail = () => {
     const { id } = useParams();
+    const eventInfo = useLoaderData();
 
     return (
         <div>
