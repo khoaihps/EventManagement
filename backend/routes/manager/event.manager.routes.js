1de10eec0685129fe741258ba92c0747f49f5a58
khoaihps <khoadoan2472003@gmail.com>
2023-08-05 23:33:27 +0700

    Task detail route

--- backend/routes/manager/event.manager.routes.js
+++ backend/routes/manager/event.manager.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { authenticateManager } = require('../../middlewares/auth.middleware');
 const { allEvents, eventDetail, allTaskOfEvent, eventUpdate } = require('../../middlewares/event.middleware');
+const { taskDetail, taskUpdate } = require("../../middlewares/task.middleware");
 const router = express.Router();
 
 router.use(authenticateManager);
@@ -9,5 +10,8 @@ router.get('/all', allEvents);
 router.get('/:eventId', eventDetail);
 router.get('/:eventId/task', allTaskOfEvent);
 router.put('/:eventId/update', eventUpdate);
+
+router.get('/task/:taskId', taskDetail);
+router.put('/task/:taskId/update', taskUpdate);
 
 module.exports = router;
