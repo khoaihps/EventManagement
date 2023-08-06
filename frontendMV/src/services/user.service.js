17b64ec8277b22f5545f533c53d69408ca804701
An <antp9254@gmail.com>
2023-08-06 23:26:20 +0700

    update updateCustomerInfo

--- frontendMV/src/services/user.service.js
+++ frontendMV/src/services/user.service.js
@@ -1,5 +1,6 @@
 import authHeader from "./auth-header";
 import AuthService from "./auth.service";
+import { convertToISODate } from "./util";
 
 const API_URL = "http://localhost:4000";
 
@@ -19,50 +20,34 @@ const getCustomerInfo = async () => {
   }
 };
 
-// export async function update(firstName, lastName, username, password, DOB, email, phone, address) {
-//     try {
-//         const isoDate = convertToISODate(DOB)
-//         console.log(username);
-//         const response = await fetch(`${API_URL}/register/customer`, {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({ firstName, lastName, username, password, DOB: isoDate, email, phone, address }),
-//         });
-
-//         if (response.ok) {
-//             const data = await response.json();
-//             return { success: true, name: data.name};
-//         } else {
-//             const errorData = await response.json();
-//             return { success: false, error: errorData.message };
-//         }
-//     } catch (error) {
-//         console.error('An error occurred during login:', error);
-//         return { success: false, error: 'An error occurred during register' };
-//     }
-// }
-
-const updateCustomerInfo = async () => {
+const updateCustomerInfo = async (
+  firstName,
+  lastName,
+  DOB,
+  email,
+  phone,
+  address
+) => {
   try {
     const customerID = AuthService.getCurrentUser().customerID;
-    const response = await fetch(API_URL + "customer/profile/update" + customerID, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        // firstName,
-        // lastName,
-        // username,
-        // password,
-        // DOB: isoDate,
-        // email,
-        // phone,
-        // address,
-      }),
-    });
+    const isoDate = convertToISODate(DOB);
+    const response = await fetch(
+      API_URL + "customer/profile/update" + customerID,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          DOB: isoDate,
+          email,
+          phone,
+          address
+        }),
+      }
+    );
     if (response.ok) {
       const data = await response.json();
       return data;
