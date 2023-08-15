d797456e118c86e002f7336c488ebb6f9f84cb73
khoaihps <khoadoan2472003@gmail.com>
2023-08-16 00:10:33 +0700

    Features: Edit employees 2

--- frontendTA/src/Components/event/employee/employeeDetail/EmployeeInfo.js
+++ frontendTA/src/Components/event/employee/employeeDetail/EmployeeInfo.js
@@ -13,7 +13,7 @@ const EmployeeInfo = ({employee, isEditable}) => {
                                // onChange={}
                         />
                         <label htmlFor="floating_email"
-                               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
+                               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Username</label>
                     </div>
                     <div className="grid md:grid-cols-2 md:gap-6">
                         <div className="relative z-0 w-full mb-6 group">
