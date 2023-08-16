2886b961b597b64dc017c8ab06c7adb1e01727f2
khoaihps <khoadoan2472003@gmail.com>
2023-08-16 21:40:46 +0700

    Revert "mindvu update"
    This reverts commit c50bd26f5b2122f0c6e2594da3aeba5f15d27684.

--- frontendTA/src/services/event.service.js
+++ frontendTA/src/services/event.service.js
@@ -1,119 +1,150 @@
 import axios from "axios";
 import authHeader from "./auth-header";
-import AuthService from "./auth.service";
 
-const API_URL = "http://localhost:4000/";
-const role = AuthService.getCurrentUser().role;
+const API_URL = 'http://localhost:4000/';
+
 const getEventInfo = async (eventID) => {
-  try {
-    const response = await fetch(API_URL + `${role}` + "/event/" + eventID, {
-      method: "GET",
-      headers: authHeader(),
-    });
-    if (response.ok) {
-      const data = await response.json();
-      return data;
+    try {
+        const response = await fetch(API_URL + "manager/event/" + eventID, {
+            method: 'GET',
+            headers: authHeader(),
+        });
+        if (response.ok){
+            const data = await response.json();
+            return data;
+        }
+    } catch (error) {
+        console.log("Error: ", error);
     }
-  } catch (error) {
-    console.log("Error: ", error);
-  }
-};
+}
 const getAllEvent = async () => {
-  try {
-    const response = await fetch(API_URL + `${role}` + "/event/all", {
-      method: "GET",
-      headers: authHeader(),
-    });
-    if (response.ok) {
-      const data = await response.json();
-      return data;
+    try {
+        const response = await fetch(API_URL + "manager/event/all", {
+            method: 'GET',
+            headers: authHeader(),
+        });
+        if (response.ok){
+            const data = await response.json();
+            return data;
+        }
+    } catch (error) {
+        console.log("Error: ", error);
     }
-  } catch (error) {
-    console.log("Error: ", error);
-  }
-};
+}
 const getAllOpenEvent = async () => {
-  try {
-    const response = await fetch(API_URL + `${role}` + "/event/all", {
-      method: "GET",
-      headers: authHeader(),
-    });
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      return data;
+    try {
+        const response = await fetch(API_URL + "team-member/event/all", {
+            method: 'GET',
+            headers: authHeader(),
+        });
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            return data;
+        }
+    } catch (error) {
+        console.log("Error: ", error);
     }
-  } catch (error) {
-    console.log("Error: ", error);
-  }
-};
+}
 const getRegisteredEmployees = async (eventID) => {
-  try {
-    const response = await fetch(
-      API_URL + "manager/event/" + eventID + "/remployees",
-      {
-        method: "GET",
-        headers: authHeader(),
-      }
-    );
-    if (response.ok) {
-      const data = await response.json();
-      return data;
+    try {
+        const response = await fetch(API_URL + "manager/event/" + eventID + "/remployees", {
+            method: 'GET',
+            headers: authHeader(),
+        });
+        if (response.ok){
+            const data = await response.json();
+            return data;
+        }
+    } catch (error) {
+        console.log("Error: ", error);
     }
-  } catch (error) {
-    console.log("Error: ", error);
-  }
-};
+}
 const getUnregisteredEmployees = async (eventID) => {
-  try {
-    const response = await fetch(
-      API_URL + "manager/event/" + eventID + "/uremployees",
-      {
-        method: "GET",
-        headers: authHeader(),
-      }
-    );
-    if (response.ok) {
-      const data = await response.json();
-      return data;
+    try {
+        const response = await fetch(API_URL + "manager/event/" + eventID + "/uremployees", {
+            method: 'GET',
+            headers: authHeader(),
+        });
+        if (response.ok){
+            const data = await response.json();
+            return data;
+        }
+    } catch (error) {
+        console.log("Error: ", error);
     }
-  } catch (error) {
-    console.log("Error: ", error);
-  }
-};
+}
 
 const updateEvent = async (eventId, updatedEventDetails) => {
-  try {
-    console.log(updatedEventDetails);
-    const response = await fetch(
-      API_URL + "manager/event/" + eventId + "/update",
-      {
-        method: "PUT",
-        headers: {
-          ...authHeader(),
-          "Content-Type": "application/json", // Set the Content-Type header
-        },
-        body: JSON.stringify(updatedEventDetails),
-      }
-    );
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      return data;
+    try {
+        const response = await fetch(API_URL + "manager/event/"+ eventId + "/update", {
+            method: 'PUT', 
+            headers: {
+                ...authHeader(),
+                'Content-Type': 'application/json' // Set the Content-Type header
+            },
+            body: JSON.stringify(updatedEventDetails) 
+        });
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            return data;
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+        throw error;
     }
-  } catch (error) {
-    console.log("Error: ", error);
-    throw error;
-  }
-};
+}
+
+const addEventRegister = async (eventId, t_member_id) => {
+    try {
+        const response = await fetch(API_URL + "manager/event/"+ eventId + "/eventregister/add", {
+            method: 'POST', 
+            headers: {
+                ...authHeader(),
+                'Content-Type': 'application/json' // Set the Content-Type header
+            },
+            body: JSON.stringify({t_member_id: t_member_id}) 
+        });
+        if (response.ok){
+            const data = await response.json();
+            return data;
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+        throw error;
+    }
+}
+
+const removeEventRegister = async (eventId, t_member_id) => {
+    try {
+        const response = await fetch(API_URL + "manager/event/"+ eventId + "/eventregister/remove", {
+            method: 'DELETE', 
+            headers: {
+                ...authHeader(),
+                'Content-Type': 'application/json' // Set the Content-Type header
+            },
+            body: JSON.stringify({t_member_id: t_member_id}) 
+        });
+        if (response.ok){
+            const data = await response.json();
+            return data;
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+        throw error;
+    }
+}
 
 const EventService = {
-  getEventInfo,
-  getAllEvent,
-  getAllOpenEvent,
-  getRegisteredEmployees,
-  getUnregisteredEmployees,
-  updateEvent,
+    getEventInfo,
+    getAllEvent,
+    getAllOpenEvent,
+    getRegisteredEmployees,
+    getUnregisteredEmployees,
+    updateEvent,
+    addEventRegister,
+    removeEventRegister
 };
 
 export default EventService;
