8f0f624bfecf27509ccf0af59fa87ef4a5a46cf9
phuthuydz2k3 <hnhtr1999@gmail.com>
2023-08-17 13:33:44 +0700

    asdfasdf
    sdaffd

--- frontendTA/src/Components/event/EventTable.jsx
+++ frontendTA/src/Components/event/EventTable.jsx
@@ -46,7 +46,7 @@ const EventTable = ({ event, isEditable, change}) => {
                                 } block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer`}
                                 required
                                 value={event.status}
-                                readOnly={!isEditable}
+                                disabled={!isEditable}
                                 onChange={handleChange}
                             >
                                 <option value="pending">Pending</option>
--- frontendTA/src/Components/event/task/taskDetail/TaskInfo.js
+++ frontendTA/src/Components/event/task/taskDetail/TaskInfo.js
@@ -65,7 +65,7 @@ const TaskInfo = ({task, setTask, isEditable}) => {
                                     id="floating_repeat_password"
                                     className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer`}
                                     required
-                                    readOnly={!isEditable}
+                                    disabled={!isEditable}
                                     value={task.status}
                                     onChange={handleChange}
                                 >
